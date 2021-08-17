343af2557e439befc47476b6de86728ef5bb15bd
“zhouxq” <zhouxq@cdmtc.com>
2021-08-17 10:09:53 +0800

    删除无用文件

--- geetest_slider/tt.js
+++
@@ -1,9 +0,0 @@
-const jsdom = require("jsdom");
-const fs = require('fs');
-const { JSDOM } = jsdom;
-const { createCanvas } = require('canvas');
-
-
-const html = fs.readFileSync('./index.html', 'utf8');
-const dom = new JSDOM(html);
-window = dom.window;
